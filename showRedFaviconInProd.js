2d53c1f15deeaab65284a22608bbd84c886d3abb
Andy Rumpler <amrumpler@gmail.com>
2020-06-05 11:16:38 -0500

    wait additional time so icon isn't overwritten

--- showRedFaviconInProd.js
+++ showRedFaviconInProd.js
@@ -7,8 +7,13 @@
 // @grant    GM_addStyle
 // ==/UserScript==
 //- The @grant directive is needed to restore the proper sandbox.
-console.log('editing page');
-waitForKeyElements ("link[rel='icon']", updateFavicon);
+
 function updateFavicon (jNode) {
-    jNode.attr('href', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAD5SURBVDhPY3wro/KfgQLABKXJBrQzoKRlPcOXi5cY+B/eYrh18DRDQN5SqAwqwAiDoPylDLwcLAyr0vQZONmYoaIMDNeef2Hwn3qe4eTCJKgIBKC4oHfyNob9xWYMm3OMUDSDgJYkD4OxPB+UhwAoBvgbiDEwMkI5WEC+kzxDZt0aKA8C4Aa4pC9i4GLFH6aWygIMK1L0GHombYOKIIXB1X2nGGxVBcGChMC7r78ZGDS0wGy4ldZA04kFXEjhAzfg4pPPUBZhsOPqaygLyYCydbcYLj/9zPD/P+6U/evPP4bNl14xNGy+CxXBkg5IBfiDnQgw0AYwMAAA9NtDeiwDaJcAAAAASUVORK5CYII=');
+    setTimeout(function() {
+        console.log('updating');
+        jNode.attr('href', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAD5SURBVDhPY3wro/KfgQLABKXJBrQzoKRlPcOXi5cY+B/eYrh18DRDQN5SqAwqwAiDoPylDLwcLAyr0vQZONmYoaIMDNeef2Hwn3qe4eTCJKgIBKC4oHfyNob9xWYMm3OMUDSDgJYkD4OxPB+UhwAoBvgbiDEwMkI5WEC+kzxDZt0aKA8C4Aa4pC9i4GLFH6aWygIMK1L0GHombYOKIIXB1X2nGGxVBcGChMC7r78ZGDS0wGy4ldZA04kFXEjhAzfg4pPPUBZhsOPqaygLyYCydbcYLj/9zPD/P+6U/evPP4bNl14xNGy+CxXBkg5IBfiDnQgw0AYwMAAA9NtDeiwDaJcAAAAASUVORK5CYII=');
+    }, 2000); //Two seconds will elapse and Code will execute.
 }
+
+waitForKeyElements ("link[rel='icon']", updateFavicon, false);
+
